ae4eb502fc0b093a12b50a21c8c4260814101a43
TotalTaxAmount <shieldscoen@gmail.com>
2024-02-28 13:45:49 -0700

    feature: Forward comms ports. Also change some patch, /dev/mtd5 might be FPGA??

--- run_vm.sh
+++ run_vm.sh
@@ -13,7 +13,7 @@ qemu-system-arm \
   -kernel linux/uImage -dtb linux/devicetree.dtb \
   -display none -serial null -serial mon:stdio \
   -append "clocksource=hpet init=/sbin/init console=ttyPS0,115200 earlyprintk root=/dev/mmcblk0 rw" \
-  -net user,hostfwd=tcp::${LOCAL_SSH_PORT}-:22 \
+  -net user,hostfwd=tcp::${LOCAL_SSH_PORT}-:22,hostfwd=tcp::1741-:1741,hostfwd=tcp::1742-:1742,hostfwd=udp::1164-:1164,hostfwd=udp::1166-:1166\
   -net nic \
   -rtc base=localtime,clock=vm\
   -sd "$IMG_FILE"
