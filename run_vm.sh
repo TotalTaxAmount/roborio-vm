1d3126bcdcec40543da165839a529d4851944dab
cmd / Ada <79297197+cmdada@users.noreply.github.com>
2024-03-05 14:33:32 -0700

    Most clock source errors (fpga excluded) go away with these params

--- run_vm.sh
+++ run_vm.sh
@@ -23,7 +23,7 @@ $QEMU \
   -machine xilinx-zynq-a9 -cpu cortex-a9 -m $RAM_SIZE \
   -kernel linux/uImage -dtb linux/devicetree.dtb \
   -display none -serial null -serial mon:stdio \
-  -append "clocksource=hpet init=/sbin/init console=ttyPS0,115200 earlyprintk root=/dev/mmcblk0 rw" \
+  -append "-rtc base=localtime,clock=vm -icount shift=7,align=off,sleep=off init=/sbin/init console=ttyPS0,115200 earlyprintk root=/dev/mmcblk0 rw" \
   -net user,hostfwd=tcp::${LOCAL_SSH_PORT}-:22,hostfwd=tcp::1741-:1741,hostfwd=tcp::1742-:1742,hostfwd=udp::1164-:1164,hostfwd=udp::1166-:1166\
   -net nic \
   -rtc base=localtime,clock=vm\
