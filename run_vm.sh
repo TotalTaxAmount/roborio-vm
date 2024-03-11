7ccd75cba98e9703f85a6f20f985b0552824aaa2
TotalTaxAmount <shieldscoen@gmail.com>
2024-03-11 10:59:01 -0600

    fix: error about /boot/.safe after startup has completed

--- run_vm.sh
+++ run_vm.sh
@@ -18,15 +18,14 @@ if [ ! -f ../qemu/build/qemu-system-arm ] && [ $QEMU == "../qemu/build/qemu-syst
     exit 1
 fi
 
-
 $QEMU \
   -machine xilinx-zynq-a9 -cpu cortex-a9 -m $RAM_SIZE \
   -kernel linux/uImage -dtb linux/devicetree.dtb \
   -display none -serial null -serial mon:stdio \
-  -append "clocksource=hpet init=/sbin/init console=ttyPS0,115200 earlyprintk root=/dev/mmcblk0 rw" \
+  -append "cgroup_enable=memory clocksource=hpet init=/sbin/init console=ttyPS0,115200 earlyprintk root=/dev/mmcblk0 rw" \
   -net user,hostfwd=tcp::${LOCAL_SSH_PORT}-:22,hostfwd=tcp::1741-:1741,hostfwd=tcp::1742-:1742,hostfwd=udp::1164-:1164,hostfwd=udp::1166-:1166\
   -net nic \
-  -rtc base=localtime,clock=vm\
+  -rtc base=localtime,clock=vm \
   -sd "$IMG_FILE"
 
 # TODO: how to safely shutdown the system?
