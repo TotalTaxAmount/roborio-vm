82f30019c3a3c80e47026529b135311bf7048377
TotalTaxAmount <shieldscoen@gmail.com>
2024-03-15 15:09:25 -0600

    feature: get mDNS to work (almost)

--- run_vm.sh
+++ run_vm.sh
@@ -18,12 +18,13 @@ if [ ! -f ../qemu/build/qemu-system-arm ] && [ $QEMU == "../qemu/build/qemu-syst
     exit 1
 fi
 
+# TODO: Figure out how to forward mDNS port 5353 without breaking avahi
 $QEMU \
   -machine xilinx-zynq-a9 -cpu cortex-a9 -m $RAM_SIZE \
   -kernel linux/uImage -dtb linux/devicetree.dtb \
   -display none -serial null -serial mon:stdio \
   -append "-rtc base=localtime,clock=vm -icount shift=7,align=off,sleep=off init=/sbin/init console=ttyPS0,115200 earlyprintk root=/dev/mmcblk0 rw" \
-  -net user,hostfwd=tcp::${LOCAL_SSH_PORT}-:22,hostfwd=tcp::1741-:1741,hostfwd=tcp::1742-:1742,hostfwd=udp::1164-:1164,hostfwd=udp::1166-:1166\
+  -net user,hostfwd=tcp::${LOCAL_SSH_PORT}-:22,hostfwd=tcp::1741-:1741,hostfwd=tcp::1742-:1742,hostfwd=udp::1164-:1164,hostfwd=udp::1166-:1166 \
   -net nic \
   -sd "$IMG_FILE"
 
